3d27f5d5fbcb75a70a6e84460fdb713db76bba0c
TadeyshD <verydeadman03@gmail.com>
2022-03-10 17:25:38 +0300

    Start

--- src/com/company/Lesson_1/MotivationSystem.java
+++
@@ -1,7 +0,0 @@
-package com.company.Lesson_1;
-
-public interface MotivationSystem {
-    void getBonus();
-
-    void getBonus(int extraBonus);
-}
--- src/com/company/Lesson_2/Lesson_2_2.java
+++ src/com/company/Lesson_2/Lesson_2_2.java
@@ -1,9 +1,37 @@
 package com.company.Lesson_2;
 
+import java.util.Scanner;
+
 public class Lesson_2_2 {
     public static void main(String[] args) {
-        String[] strings = {"Hello", "Bonjure", "Привет"};
-        System.out.println(strings[3]);
+        Scanner scanner = new Scanner(System.in);
+        String variant;
+        do {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            variant = scanner.next();
 
-    }
+        if (variant.equals("1")){ //equals - сравнение
+            System.out.println(a + b);
+        } else if (variant.equals("2")){
+            System.out.println(a -b);
+        } else if (variant.equals("3")){
+            System.out.println(a * b);
+        } else if (variant.equals("4")) {
+            try {
+                System.out.println(a / b);
+            } catch (ArithmeticException ae) { //создаём исключение
+                System.out.println("Вы сделали хрень");
+            }
+        }else if (variant.equals("5")){
+            break;
+        } else {
+            System.out.println("Вы ввели не то");
+        }
+
+        }
+        while (true);
+        System.out.println("Спасибо за драники.");
+        System.out.println("Поддержи проект и дай пять тыщ.");
+        }
 }
